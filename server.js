2357c6a962a81a7610cacfc1209575847c730e2e
SRIDHARAN <sridharan18122002@gmail.com>
2026-03-01 12:03:43 +0530

    main

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",  // React Vite port
+  origin: "https://ai-missing-finder-frontend-2026.vercel.app",  // React Vite port
   credentials: true
 }));
 
